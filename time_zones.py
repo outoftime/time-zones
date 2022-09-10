8de9cf71eb84883ead880e7dff1ce46b7b394e83
Mat Brown <matbrown@fastmail.com>
2022-09-10 07:09:19 -0400

    Function to read shape records

--- time_zones.py
+++ time_zones.py
@@ -1,11 +1,22 @@
 import sys
 import shapefile
 
-sf = shapefile.Reader(sys.argv[1])
 
-for record in sf.iterRecords():
-    state = record[0]
-    county = record[2]
-    lon = record[6]
-    lat = record[7]
-    print(county + ", " + state + ": (" + str(lat) + "," + str(lon) + ")")
+def load_county_centroids(file_path):
+    sf = shapefile.Reader(file_path)
+
+    for record in sf.iterRecords():
+        yield dict(state=record[0], county=record[2], lon=record[6], lat=record[7])
+
+
+for record in load_county_centroids(sys.argv[1]):
+    print(
+        record["county"]
+        + ", "
+        + record["state"]
+        + ": ("
+        + str(record["lat"])
+        + ","
+        + str(record["lon"])
+        + ")"
+    )
