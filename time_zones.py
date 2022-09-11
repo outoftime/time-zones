e6459a0ce401707b35f49987a67d9f49ecdb73dd
Mat Brown <matbrown@fastmail.com>
2022-09-11 16:31:03 -0400

    Calculate optimal time zone

--- time_zones.py
+++ time_zones.py
@@ -59,6 +59,32 @@ STATE_ABBREVIATIONS = {
 }
 
 
+def gmt_solar_noon(lon):
+    return 12 - ((lon / 15) % 24)
+
+
+def cost(solar_noon):
+    if solar_noon > 13:
+        return solar_noon - 13
+    if solar_noon < 12:
+        return 12 - solar_noon
+    return 0
+
+
+def state_cost(county_records, offset):
+    total_cost = 0
+    for record in county_records:
+        local_solar_noon = (gmt_solar_noon(record["lon"]) + offset) % 24
+        unadjusted_cost = cost(local_solar_noon)
+        county_cost = unadjusted_cost * record["population"]
+        total_cost += county_cost
+    return total_cost
+
+
+def best_gmt_offset(county_records):
+    return min(range(-12, 12), key=lambda offset: state_cost(county_records, offset))
+
+
 def normalize_county(county):
     in_match = re.match(".+ in (.+)", county)
     if in_match:
@@ -89,7 +115,7 @@ def load_county_populations(file_path):
         next(csv_file)
         for row in csv_file:
             if row[4] != "000":  # state total
-                yield dict(state=row[5], county=row[6], population=row[9])
+                yield dict(state=row[5], county=row[6], population=float(row[9]))
 
 
 def merge_county_records(county_centroids, county_populations):
@@ -128,11 +154,24 @@ def merge_county_records(county_centroids, county_populations):
     return grouped_merged_records
 
 
+def optimal_time_zones(county_records_by_state):
+    time_zones = defaultdict(lambda: [])
+
+    for state, county_records in county_records_by_state.items():
+        optimal_offset = best_gmt_offset(county_records)
+        time_zones[optimal_offset].append(state)
+
+    return time_zones
+
+
 if __name__ == "__main__":
     county_centroids = load_county_centroids(sys.argv[1])
 
     county_populations = load_county_populations(sys.argv[2])
 
-    for a, b in merge_county_records(county_centroids, county_populations).items():
-        print(a)
-        print(b)
+    county_records_by_state = merge_county_records(county_centroids, county_populations)
+    for offset, states in optimal_time_zones(county_records_by_state).items():
+        print("Offset: GMT" + str(offset))
+        for state in states:
+            print("* " + state)
+        print("")
